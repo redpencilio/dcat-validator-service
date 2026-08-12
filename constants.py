88bd972912ed91f2fa19c0c774d0ca406b42d3c5
Ruben Simons <rubensimons442@gmail.com>
2026-08-12 16:24:46 +0200

    fixed v3.0.0 SHACL URLS

--- constants.py
+++ constants.py
@@ -31,21 +31,21 @@ TARGET_CLASS_SUMMARY_URI_PREFIX = "http://redpencil.data.gift/id/target-class-su
 RULE_SUMMARY_URI_PREFIX = "http://redpencil.data.gift/id/rule-summary/"
 
 MOBILITY_DCAT_AP_VERSION = "1.1.0"
-SHACL_BASE_URL = f"https://raw.githubusercontent.com/mobilityDCAT-AP/mobilityDCAT-AP/gh-pages/releases/{MOBILITY_DCAT_AP_VERSION}/shaclShapes"
+SHACL_BASE_URL = "https://raw.githubusercontent.com/mobilityDCAT-AP/mobilityDCAT-AP/gh-pages"
 
 # The files dont follow the same pattern across versions (1.1.0 has 3 files, 3.0.0 merges shapes and vocabularies into one file)
 if MOBILITY_DCAT_AP_VERSION == "1.1.0":
     # You can find the shacl files for 1.1.0 here:
     # https://github.com/mobilityDCAT-AP/mobilityDCAT-AP/tree/gh-pages/releases/1.1.0/shaclShapes
     SHACL_FILES = [
-        "mobilitydcat-ap_1.1.0_shacl_shapes.ttl",
-        "mobilitydcat-ap_1.1.0_shacl_range.ttl",
-        "mobilitydcat-ap_1.1.0_shacl_mdr-vocabularies.shape.ttl"
+        "/releases/1.1.0/shaclShapes/mobilitydcat-ap_1.1.0_shacl_shapes.ttl",
+        "/releases/1.1.0/shaclShapes/mobilitydcat-ap_1.1.0_shacl_range.ttl",
+        "/releases/1.1.0/shaclShapes/mobilitydcat-ap_1.1.0_shacl_mdr-vocabularies.shape.ttl"
     ]
     # You can find the shacl files for 3.0.0 here:
     # https://github.com/mobilityDCAT-AP/mobilityDCAT-AP/tree/gh-pages/drafts/latest/shaclShapes
 elif MOBILITY_DCAT_AP_VERSION == "3.0.0":
     SHACL_FILES = [
-        "mobilitydcat-ap-shacl.ttl",
-        "mobilitydcat-ap-shacl-ranges.ttl"
+        "/drafts/latest/shaclShapes/mobilitydcat-ap-shacl.ttl",
+        "/drafts/latest/shaclShapes/mobilitydcat-ap-shacl-ranges.ttl"
     ]
