78ceb6eb281fb472bc44dff7fdbcc962381ff6cf
Ruben Simons <rubensimons442@gmail.com>
2026-08-17 15:30:08 +0200

    Moved mobilitydcat-ap spec related stuff to new spec.py and used existing spec dictionary to attach severity to vocabulary violations

---
+++ spec.py
@@ -0,0 +1,159 @@
+from enum import Enum
+
+DCAT_CLASSES = [
+    "http://www.w3.org/ns/dcat#Catalog",
+    "http://www.w3.org/ns/dcat#Dataset",
+    "http://www.w3.org/ns/dcat#Distribution",
+    "http://www.w3.org/ns/dcat#CatalogRecord",
+]
+
+MOBILITY_DCAT_AP_VERSION = "1.1.0"
+
+SHACL_BASE_URL = (
+    "https://raw.githubusercontent.com/mobilityDCAT-AP/mobilityDCAT-AP/gh-pages"
+)
+
+# The files dont follow the same pattern across versions (1.1.0 has 3 files, 3.0.0 merges shapes and vocabularies into one file)
+if MOBILITY_DCAT_AP_VERSION == "1.1.0":
+    # You can find the shacl files for 1.1.0 here:
+    # https://github.com/mobilityDCAT-AP/mobilityDCAT-AP/tree/gh-pages/releases/1.1.0/shaclShapes
+    SHACL_FILES = [
+        "/releases/1.1.0/shaclShapes/mobilitydcat-ap_1.1.0_shacl_shapes.ttl",
+        "/releases/1.1.0/shaclShapes/mobilitydcat-ap_1.1.0_shacl_range.ttl",
+        # "/releases/1.1.0/shaclShapes/mobilitydcat-ap_1.1.0_shacl_mdr-vocabularies.shape.ttl" # This is done via python scripts now
+    ]
+    # You can find the shacl files for 3.0.0 here:
+    # https://github.com/mobilityDCAT-AP/mobilityDCAT-AP/tree/gh-pages/drafts/latest/shaclShapes
+elif MOBILITY_DCAT_AP_VERSION == "3.0.0":
+    SHACL_FILES = [
+        "/drafts/latest/shaclShapes/mobilitydcat-ap-shacl.ttl",
+        "/drafts/latest/shaclShapes/mobilitydcat-ap-shacl-ranges.ttl",
+    ]
+
+
+class Requirement(str, Enum):
+    MANDATORY = "mandatory"
+    RECOMMENDED = "recommended"
+    OPTIONAL = "optional"
+
+
+SEVERITY = {
+    Requirement.MANDATORY: "http://www.w3.org/ns/shacl#Violation",
+    Requirement.RECOMMENDED: "http://www.w3.org/ns/shacl#Warning",
+    Requirement.OPTIONAL: "http://www.w3.org/ns/shacl#Info",
+}
+
+# mobilityDCAT-AP spec. Source:
+# https://mobilitydcat-ap.github.io/mobilityDCAT-AP/releases/index.html
+MOBILITY_DCAT_AP_SPEC = {
+    "http://www.w3.org/ns/dcat#Catalog": {
+        Requirement.MANDATORY: [
+            "http://purl.org/dc/terms/description",
+            "http://purl.org/dc/terms/spatial",
+            "http://xmlns.com/foaf/0.1/homepage",
+            "http://purl.org/dc/terms/publisher",
+            "http://www.w3.org/ns/dcat#record",
+            "http://purl.org/dc/terms/title",
+        ],
+        Requirement.RECOMMENDED: [
+            "http://purl.org/dc/terms/language",
+            "http://purl.org/dc/terms/license",
+            "http://purl.org/dc/terms/modified",
+            "http://purl.org/dc/terms/issued",
+            "http://www.w3.org/ns/dcat#themeTaxonomy",
+        ],
+        Requirement.OPTIONAL: [
+            "http://www.w3.org/ns/dcat#dataset",
+            "http://purl.org/dc/terms/hasPart",
+            "http://purl.org/dc/terms/identifier",
+            "http://www.w3.org/ns/adms#identifier",
+        ],
+    },
+    "http://www.w3.org/ns/dcat#Dataset": {
+        Requirement.MANDATORY: [
+            "http://www.w3.org/ns/dcat#distribution",
+            "http://purl.org/dc/terms/description",
+            "http://purl.org/dc/terms/accrualPeriodicity",
+            "http://purl.org/dc/terms/spatial",
+            "https://w3id.org/mobilitydcat-ap#mobilityTheme",
+            "http://purl.org/dc/terms/publisher",
+            "http://purl.org/dc/terms/title",
+        ],
+        Requirement.RECOMMENDED: [
+            "https://w3id.org/mobilitydcat-ap#georeferencingMethod",
+            "http://www.w3.org/ns/dcat#contactPoint",
+            "http://www.w3.org/ns/dcat#keyword",
+            "https://w3id.org/mobilitydcat-ap#networkCoverage",
+            "http://purl.org/dc/terms/conformsTo",
+            "http://purl.org/dc/terms/rightsHolder",
+            "http://purl.org/dc/terms/temporal",
+            "http://www.w3.org/ns/dcat#theme",
+            "https://w3id.org/mobilitydcat-ap#transportMode",
+        ],
+        Requirement.OPTIONAL: [
+            "http://data.europa.eu/r5r/applicableLegislation",
+            "https://w3id.org/mobilitydcat-ap#assessmentResult",
+            "http://purl.org/dc/terms/hasVersion",
+            "http://purl.org/dc/terms/identifier",
+            "https://w3id.org/mobilitydcat-ap#intendedInformationService",
+            "http://purl.org/dc/terms/isReferencedBy",
+            "http://purl.org/dc/terms/isVersionOf",
+            "http://purl.org/dc/terms/language",
+            "http://www.w3.org/ns/adms#identifier",
+            "http://purl.org/dc/terms/relation",
+            "http://purl.org/dc/terms/issued",
+            "http://purl.org/dc/terms/modified",
+            "http://www.w3.org/2002/07/owl#versionInfo",
+            "http://www.w3.org/ns/adms#versionNotes",
+            "http://www.w3.org/ns/dqv#hasQualityAnnotation",
+        ],
+    },
+    "http://www.w3.org/ns/dcat#Distribution": {
+        Requirement.MANDATORY: [
+            "http://www.w3.org/ns/dcat#accessURL",
+            "https://w3id.org/mobilitydcat-ap#mobilityDataStandard",
+            "http://purl.org/dc/terms/format",
+            "http://purl.org/dc/terms/rights",
+        ],
+        Requirement.RECOMMENDED: [
+            "https://w3id.org/mobilitydcat-ap#applicationLayerProtocol",
+            "http://purl.org/dc/terms/description",
+            "http://purl.org/dc/terms/license",
+        ],
+        Requirement.OPTIONAL: [
+            "http://www.w3.org/ns/dcat#accessService",
+            "http://www.w3.org/2011/content#characterEncoding",
+            "https://w3id.org/mobilitydcat-ap#communicationMethod",
+            "https://w3id.org/mobilitydcat-ap#dataFormatNotes",
+            "http://www.w3.org/ns/dcat#downloadURL",
+            "https://w3id.org/mobilitydcat-ap#grammar",
+            "http://www.w3.org/ns/adms#sample",
+            "http://purl.org/dc/terms/temporal",
+        ],
+    },
+    "http://www.w3.org/ns/dcat#CatalogRecord": {
+        Requirement.MANDATORY: [
+            "http://purl.org/dc/terms/created",
+            "http://purl.org/dc/terms/language",
+            "http://purl.org/dc/terms/modified",
+            "http://xmlns.com/foaf/0.1/primaryTopic",
+        ],
+        Requirement.OPTIONAL: [
+            "http://purl.org/dc/terms/publisher",
+            "http://purl.org/dc/terms/source",
+        ],
+    },
+}
+
+PROPERTY_MAPPING = {
+    # File Stem: Predicate URI
+    "transport-mode": "https://w3id.org/mobilitydcat-ap#transportMode",
+    "mobility-theme": "https://w3id.org/mobilitydcat-ap#mobilityTheme",
+    "mobility-data-standard": "https://w3id.org/mobilitydcat-ap#mobilityDataStandard",
+    "application-layer-protocol": "https://w3id.org/mobilitydcat-ap#applicationLayerProtocol",
+    "communication-method": "https://w3id.org/mobilitydcat-ap#communicationMethod",
+    "network-coverage": "https://w3id.org/mobilitydcat-ap#networkCoverage",
+    "georeferencing-method": "https://w3id.org/mobilitydcat-ap#georeferencingMethod",
+    "intended-information-service": "https://w3id.org/mobilitydcat-ap#intendedInformationService",
+    "grammar": "https://w3id.org/mobilitydcat-ap#grammar",
+}
